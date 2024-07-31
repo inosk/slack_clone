a815fc3d189404ec250f2b254e2ced340a8ced61
inosk <inoue.yusuke.928@gmail.com>
2024-07-31 23:42:50 +0900

    workspaceの切り替えまで

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -7,6 +7,11 @@ import { Card, CardContent } from './ui/card';
 import { Separator } from './ui/separator';
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 import CreateWorkspace from './create-workspace';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import ProgressBar from './progress-bar';
+import { cn } from '@/lib/utils';
+import { useColorPreferences } from '@/providers/color-provider';
 
 type SidebarNavProps = {
   userWorkspaceData: WorkSpace[];
@@ -17,6 +22,22 @@ const SidebarNav = ({
   userWorkspaceData,
   currentWorkspaceData,
 }: SidebarNavProps) => {
+  const router = useRouter();
+  const [switchingWorkspace, setSwitcingWorkspace] = useState(false);
+  const { color } = useColorPreferences();
+
+  let backgroundColor = 'bg-primary-dark';
+  if (color === 'green') {
+    backgroundColor = 'bg-green-700';
+  } else if (color) {
+    backgroundColor = 'bg-blue-700';
+  }
+  const switchWorkspace = (id: string) => {
+    setSwitcingWorkspace(true);
+    router.push(`/workspace/${id}`);
+    setSwitcingWorkspace(true);
+  };
+
   return (
     <nav>
       <ul className="flex flex-col space-y-4 items-center">
@@ -38,35 +59,50 @@ const SidebarNav = ({
               <PopoverContent className="p-0" side="bottom">
                 <Card className="w-[350px] border-0">
                   <CardContent className="flex p-0 flex-col">
-                    {userWorkspaceData.map((workspace) => (
-                      <div
-                        key={workspace.id}
-                        className="hover:opacity-70 px-2 py-1 flex gap-2"
-                      >
-                        <Avatar>
-                          <AvatarImage
-                            src={workspace.image_url || ''}
-                            alt={workspace.name}
-                            className="object-cover w-full h-full"
-                          />
-                          <AvatarFallback className="bg-neutral-700">
-                            <Typography text={workspace.name.slice(0, 2)} />
-                          </AvatarFallback>
-                        </Avatar>
-                        <div>
-                          <Typography
-                            variant="p"
-                            text={workspace.name}
-                            className="text-sm"
-                          />
-                          <Typography
-                            variant="p"
-                            text={workspace.invite_code || ''}
-                            className="text-xs lg:text-xm"
-                          />
-                        </div>
+                    {switchingWorkspace ? (
+                      <div className="m-2">
+                        <ProgressBar />
                       </div>
-                    ))}
+                    ) : (
+                      userWorkspaceData.map((workspace) => {
+                        const isActive =
+                          workspace.id === currentWorkspaceData.id;
+
+                        return (
+                          <div
+                            key={workspace.id}
+                            className={cn(
+                              isActive && `${backgroundColor} text-white`,
+                              `cursor-pointer px-2 py-1 flex gap-2`,
+                            )}
+                            onClick={() => switchWorkspace(workspace.id)}
+                          >
+                            <Avatar>
+                              <AvatarImage
+                                src={workspace.image_url || ''}
+                                alt={workspace.name}
+                                className="object-cover w-full h-full"
+                              />
+                              <AvatarFallback className="bg-neutral-700">
+                                <Typography text={workspace.name.slice(0, 2)} />
+                              </AvatarFallback>
+                            </Avatar>
+                            <div>
+                              <Typography
+                                variant="p"
+                                text={workspace.name}
+                                className="text-sm"
+                              />
+                              <Typography
+                                variant="p"
+                                text={workspace.invite_code || ''}
+                                className="text-xs lg:text-xm"
+                              />
+                            </div>
+                          </div>
+                        );
+                      })
+                    )}
                     <Separator />
                     <CreateWorkspace />
                   </CardContent>
